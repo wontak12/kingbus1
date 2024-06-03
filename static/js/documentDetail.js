26d6b746b1a99dd32f2b36632633764e990959da
wontak1 <https://github.com/wontak12/kingbus.git>
2024-06-03 21:58:34 +0900

    삭제
    삭제

--- static/js/documentDetail.js
+++ static/js/documentDetail.js
@@ -1,41 +1,4 @@
-// function openDetailvehicleCreateDocument() {
-//     document.getElementById("detailvehicleCreateDocument").style.display = "block";
-// }
 
-// function clseDetailvehicleCreateDocument() {
-//     document.getElementById("detailvehicleCreateDocument").style.display = "none";
-// }
-
-// function openDetailInsurance(){
-//     document.getElementById("detailInsurance").style.display="block";
-// }
-
-// function closeDetailInsurance(){
-//     document.getElementById("detailInsurance").style.display="none";
-// }
-
-// function openDetailCertificate(){
-//     document.getElementById("detailCertificate").style.display="block";
-// }
-// function closeDetailCertificate(){
-//     document.getElementById("detailCertificate").style.display="none";
-// }
-
-// function openDetailInstallment(){
-//     document.getElementById("detailInstallment").style.display="block"
-// }
-
-// function closeDetailInstallment(){
-//     document.getElementById("detailInstallment").style.display="none"
-// }
-
-// function openDetailDocument(){
-//     document.getElementById("detailDocument").style.display="block";
-// }
-
-// function closeDetailDocument(){
-//     document.getElementById("detailDocument").style.display="none";
-// }
 document.querySelectorAll('.detailButton').forEach(button => {
     button.addEventListener('click', () => {
       const targetId = button.getAttribute('data-target');
